4591b4b2bcce9a6cf87877c68ad0761f1076c24c
Victor <vitek@resnet142-42.medford.tufts.edu>
2020-01-27 17:31:29 -0500

    type cases

--- ML1.py
+++ ML1.py
@@ -17,14 +17,18 @@ def calc_k_nearest_neighbors(data_NF, query_QF, K=1):
 
 # shape reported as (num_rows, num_cols)
 def split_into_train_and_test(x_all_LF, frac_test=0.5, random_state=None):
+	if type(random_state) == int:
+		random_state = np.random.RandomState(random_state)
+	elif type(random_state) == None:
+		random_state = np.random
 	num_training_instances = int((1 - frac_test) * x_all_LF.shape[0])  # floors it
 	training_set = np.zeros((num_training_instances, x_all_LF.shape[1]))
-	test_set = np.zeros(((x_all_LF.shape[0] - num_training_instances), x_all_LF.shape[1]))
-	for feature in range(num_training_instances):
-		training_row = random_state.randint(0, high= x_all_LF.shape[0])
-		training_set[feature] = x_all_LF[training_row]
-		x_all_LF = np.delete(x_all_LF, training_row, axis=0)
+	#test_set = np.zeros(((x_all_LF.shape[0] - num_training_instances), x_all_LF.shape[1]))
 	test_set = x_all_LF.copy()
+	for feature in range(num_training_instances):
+		training_row = random_state.randint(0, high= test_set.shape[0])
+		training_set[feature] = x_all_LF[training_row]
+		test_set = np.delete(test_set, training_row, axis=0) # non-global
 	return (training_set, test_set)
 
 # inputs are f-dimensional vectors
@@ -33,7 +37,8 @@ def euclidean_distance(x1, x2, f):
 	for i in range(f):
 		sum += pow((x1[i] - x2[i]), 2)
 	return math.sqrt(sum)
-
+	
+'''
 def output1(train, test):
 	print(train.shape)
 	print(test.shape)
@@ -48,9 +53,8 @@ if __name__ == '__main__':
 	x_LF = np.eye(10)
 	xcopy_LF = x_LF.copy()
 	train_MF, test_NF = split_into_train_and_test(x_LF, frac_test=0.3, random_state=np.random.RandomState(0))
-	output1(train_MF, test_NF)
+	#output1(train_MF, test_NF)
 	print(np.allclose(x_LF, xcopy_LF))
 	neighb_QKF = calc_k_nearest_neighbors(train_MF, test_NF)
 	output2(neighb_QKF)
-
-
+'''
